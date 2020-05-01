250c2aacfef505d4265795d19bfd6269b2d53013
hiroki-it <hasegawafeedshop@gmail.com>
2020-05-02 04:11:23 +0900

    Terraformで，不具合のあった記述を修正した．

--- infra/terraform/modules/vpc/output.tf
+++ infra/terraform/modules/vpc/output.tf
@@ -9,8 +9,8 @@ output "vpc_id" {
 
 // Subnet
 output "subnet_public_1a_id" {
-  value = aws_subnet.public_1a.id
+  value = aws_subnet.subnet_public_1a.id
 }
 output "subnet_public_1c_id" {
-  value = aws_subnet.public_1c.id
+  value = aws_subnet.subnet_public_1c.id
 }
