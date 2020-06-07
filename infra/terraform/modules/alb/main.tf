387cd24e3561615aec50f918e8497eb59286c816
Hiroki-IT <hasegawafeedshop@gmail.com>
2020-06-08 01:34:39 +0900

    update インデントを揃えた．

--- infra/terraform/modules/alb/main.tf
+++ infra/terraform/modules/alb/main.tf
@@ -18,16 +18,16 @@ variable "instance_app_name" {}
 # Application Load Balancer
 #==========================
 resource "aws_alb" "alb" {
-  name            = "${var.instance_app_name}-alb"
+  name               = "${var.instance_app_name}-alb"
   load_balancer_type = "application"
-  security_groups = [var.security_group_alb_id]
-  subnets         = [var.subnet_public_1a_id, var.subnet_public_1c_id]
+  security_groups    = [var.security_group_alb_id]
+  subnets            = [var.subnet_public_1a_id, var.subnet_public_1c_id]
 }
 
 resource "aws_alb_target_group" "alb_target_group" {
-  name = "${var.instance_app_name}-alb-target"
-  port = 80
-  protocol = "HTTP"
+  name        = "${var.instance_app_name}-alb-target"
+  port        = 80
+  protocol    = "HTTP"
   target_type = "instance"
-  vpc_id = var.vpc_id
+  vpc_id      = var.vpc_id
 }
