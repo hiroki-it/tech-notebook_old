ba3e5b4119a6d52e622df2f142f8977c983b4ca1
hiroki-it <hasegawafeedshop@gmail.com>
2020-06-08 01:32:26 +0900

    update Terraformで構築するECRの名前を変更した．

--- infra/terraform/modules/ecr/main.tf
+++ infra/terraform/modules/ecr/main.tf
@@ -8,11 +8,11 @@ variable "instance_app_name" {}
 # ECR
 #==============
 resource "aws_ecr_repository" "ecr_repository_builder" {
-  name = "${var.instance_app_name}_builder"
+  name                 = "${var.instance_app_name}-builder-ecr-respository"
   image_tag_mutability = "MUTABLE"
 }
 
 resource "aws_ecr_repository" "ecr_repository_www" {
-  name = "${var.instance_app_name}_www"
+  name                 = "${var.instance_app_name}-www-ecr-respository"
   image_tag_mutability = "MUTABLE"
 }
