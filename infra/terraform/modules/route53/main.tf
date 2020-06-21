9d7546bc2e8770136fd99b02c013d6c4b78c85a6
Hiroki <hasegawafeedshop@gmail.com>
2020-06-22 02:33:09 +0900

    update 変数名を変更した．

--- infra/terraform/modules/route53/main.tf
+++ infra/terraform/modules/route53/main.tf
@@ -7,9 +7,9 @@ variable "r53_record_type" {}
 variable "r53_alb_dns_name" {}
 variable "r53_alb_zone_id" {}
 
-#=============
+#==========
 # Route53
-#=============
+#==========
 resource "aws_route53_zone" "r53_zone" {
   name = var.r53_domain_name
 }
