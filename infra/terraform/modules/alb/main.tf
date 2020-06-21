adc5f3719dd665f2b7a00554c87489da3a4a3a5c
hiroki-it <hasegawafeedshop@gmail.com>
2020-06-22 02:35:01 +0900

    ALBのモジュールで，リスナーとターゲットグループを構築できるようにした．

--- infra/terraform/modules/alb/main.tf
+++ infra/terraform/modules/alb/main.tf
@@ -1,6 +1,9 @@
 #=============
 # Input Value
 #=============
+// App Name
+variable "app_name" {}
+
 // VPC
 variable "vpc_id" {}
 
@@ -11,23 +14,41 @@ variable "subnet_public_1c_id" {}
 // Security Group
 variable "security_group_alb_id" {}
 
-// EC2 Instance
-variable "instance_app_name" {}
-
-#==========================
-# Application Load Balancer
-#==========================
+#======
+# ALB
+#======
 resource "aws_alb" "alb" {
-  name               = "${var.instance_app_name}-alb"
+  name               = "${var.app_name}-alb"
   load_balancer_type = "application"
   security_groups    = [var.security_group_alb_id]
   subnets            = [var.subnet_public_1a_id, var.subnet_public_1c_id]
 }
 
+#===============
+# Target Group
+#===============
 resource "aws_alb_target_group" "alb_target_group" {
-  name        = "${var.instance_app_name}-alb-target"
-  port        = 80
+  name        = "${var.app_name}-alb-target-group"
+  port        = 80 // ALBからのルーティング時の解放ポート
   protocol    = "HTTP"
-  target_type = "instance"
+  target_type = "ip"
   vpc_id      = var.vpc_id
+  health_check {
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+    timeout             = 5
+    interval            = 10
+  }
+}
+
+#===========
+# Listener
+#===========
+resource "aws_lb_listener" "lb_listener" {
+  load_balancer_arn = aws_alb.alb.arn
+  port              = 80 // ALBの受信時の解放ポート
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_alb_target_group.alb_target_group.arn
+  }
 }
