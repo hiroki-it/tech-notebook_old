a988df2fc8c3a02471040b916558687942fb2e6b
Hiroki-IT <hasegawafeedshop@gmail.com>
2020-06-26 03:16:37 +0900

    update リポジトリ名と，タグの不変性を変更した．

--- infra/terraform/modules/ecr/main.tf
+++ infra/terraform/modules/ecr/main.tf
@@ -8,11 +8,11 @@ variable "app_name" {}
 # ECR
 #======
 resource "aws_ecr_repository" "ecr_repository_builder" {
-  name                 = "${var.app_name}-builder-respository"
-  image_tag_mutability = "MUTABLE"
+  name                 = "${var.app_name}-builder"
+  image_tag_mutability = "IMMUTABLE"
 }
 
 resource "aws_ecr_repository" "ecr_repository_www" {
-  name                 = "${var.app_name}-www-respository"
-  image_tag_mutability = "MUTABLE"
+  name                 = "${var.app_name}-www"
+  image_tag_mutability = "IMMUTABLE"
 }
