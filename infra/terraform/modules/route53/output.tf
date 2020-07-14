97f06f03fc8ae97faa4f8c32e9df0f28f4c23ce2
Hiroki-IT <hasegawafeedshop@gmail.com>
2020-07-15 03:47:44 +0900

    update Route53のリソースを構築できるようにした．

---
+++ infra/terraform/modules/route53/output.tf
@@ -0,0 +1,9 @@
+#====================
+# Output From Module
+#====================
+output "route53_record_fqdn" {
+  value = aws_route53_record.route53_record.fqdn
+}
+output "route53_zone_id" {
+  value = aws_route53_zone.route53_zone.zone_id
+}
