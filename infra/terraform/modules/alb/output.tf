d16e8e036f07495951d4406ec28e51439ae500d9
Hiroki-IT <hasegawafeedshop@gmail.com>
2020-07-15 03:52:30 +0900

    update Blue/Greenデプロイメントに対応したALBのリソースを構築できるようにした．

--- infra/terraform/modules/alb/output.tf
+++ infra/terraform/modules/alb/output.tf
@@ -1,12 +1,26 @@
 #====================
 # Output From Module
 #====================
+// ALB
 output "alb_dns_name" {
-  value = aws_alb.alb.dns_name
+  value = aws_lb.alb.dns_name
 }
 output "alb_zone_id" {
-  value = aws_alb.alb.zone_id
+  value = aws_lb.alb.zone_id
 }
-output "alb_target_group_arn" {
-  value = aws_alb_target_group.alb_target_group.arn
+
+// ALB Target Group
+output "alb_target_group_green_name" {
+  value = aws_lb_target_group.alb_target_group_green.name
+}
+output "alb_target_group_blue_name" {
+  value = aws_lb_target_group.alb_target_group_blue.name
+}
+output "alb_target_group_blue_arn" {
+  value = aws_lb_target_group.alb_target_group_blue.arn
+}
+
+// Listener
+output "alb_listener_blue_arn" {
+  value = aws_lb_listener.lb_listener_blue.arn
 }
