debc8b89932a0cfb57552eb31d56382d12a66cb0
Hiroki <hasegawafeedshop@gmail.com>
2020-07-16 09:24:13 +0900

    update ホストゾーンは，リソースで作成せずに，データリソースを使用するようにした．

--- infra/terraform/modules/route53/output.tf
+++ infra/terraform/modules/route53/output.tf
@@ -5,5 +5,5 @@ output "route53_record_fqdn" {
   value = aws_route53_record.route53_record.fqdn
 }
 output "route53_zone_id" {
-  value = aws_route53_zone.route53_zone.zone_id
+  value = data.aws_route53_zone.route53_zone.id
 }
