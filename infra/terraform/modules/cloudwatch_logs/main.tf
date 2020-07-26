6ef238cc3de1d431ecdce13378c0fd765207483b
Hiroki <hasegawafeedshop@gmail.com>
2020-07-27 02:25:11 +0900

    fix 誤っている実装を修正した．

--- infra/terraform/modules/cloudwatch_logs/main.tf
+++ infra/terraform/modules/cloudwatch_logs/main.tf
@@ -2,6 +2,6 @@
 # ECS
 #======
 resource "aws_cloudwatch_log_group" "cloudwatch_log_group_ecs_" {
-  name              = "/ecs/tech-notebook/www-container/awslogs-group"
+  name              = "/aws/ecs/containerinsights/tech-notebook-ecs-cluster/performance"
   retention_in_days = 1
 }
