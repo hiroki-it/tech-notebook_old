8f74002c31e4dd71e8fdcb333c8938edcea31115
hiroki-it <hasegawafeedshop@gmail.com>
2020-07-27 00:38:15 +0900

    fix 誤っている実装を修正した．

--- infra/terraform/modules/ecr/main.tf
+++ infra/terraform/modules/ecr/main.tf
@@ -7,11 +7,6 @@ variable "app_name" {}
 #======
 # ECR
 #======
-resource "aws_ecr_repository" "ecr_repository_builder" {
-  name                 = "${var.app_name}-builder"
-  image_tag_mutability = "IMMUTABLE"
-}
-
 resource "aws_ecr_repository" "ecr_repository_www" {
   name                 = "${var.app_name}-www"
   image_tag_mutability = "IMMUTABLE"
