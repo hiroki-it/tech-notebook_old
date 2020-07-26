c79c233a021547b2bfcca800c37f02af1d349839
Hiroki <hasegawafeedshop@gmail.com>
2020-07-27 03:00:33 +0900

    update ロググループはterraformから作成しないようにした．

--- infra/terraform/modules/cloudwatch_logs/main.tf
+++ infra/terraform/modules/cloudwatch_logs/main.tf
@@ -1,7 +1,6 @@
 #======
 # ECS
 #======
-resource "aws_cloudwatch_log_group" "cloudwatch_log_group_ecs_" {
-  name              = "/aws/ecs/containerinsights/tech-notebook-ecs-cluster/performance"
-  retention_in_days = 1
+data "aws_cloudwatch_log_group" "cloudwatch_log_group_ecs_" {
+  name  = "/aws/ecs/containerinsights/tech-notebook-ecs-cluster/performance"
 }
