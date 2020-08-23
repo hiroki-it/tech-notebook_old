dca1965f4b65b2df96327ea03d1b3e80d586dd14
Hiroki-IT <hasegawafeedshop@gmail.com>
2020-08-24 00:03:13 +0900

    refactor 変数名と定数値のリファクタリングを行った．

--- infra/terraform/modules/security_group/output.tf
+++ infra/terraform/modules/security_group/output.tf
@@ -2,11 +2,11 @@
 # Output From Module
 #====================
 // ALB
-output "security_group_alb_id" {
-  value = aws_security_group.security_group_alb.id
+output "sg_alb_id" {
+  value = aws_security_group.sg_alb.id
 }
 
 // ECS
-output "security_group_ecs_id" {
-  value = aws_security_group.security_group_ecs.id
+output "sg_ecs_id" {
+  value = aws_security_group.sg_ecs.id
 }
