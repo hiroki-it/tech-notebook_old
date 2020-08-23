e751755e5c58438ab3c93a466b2022321a0b6169
Hiroki-IT <hasegawafeedshop@gmail.com>
2020-08-24 00:08:06 +0900

    update ルートモジュールファイルで使用する定数について，データ型を宣言するようにした．

--- infra/terraform/production/variables.tf
+++ infra/terraform/production/variables.tf
@@ -2,37 +2,53 @@
 # Input Value
 #=============
 // AWS認証情報
-variable "credential" {}
+variable "credential" {
+  type = map(string)
+}
 
 // App Name
-variable "app_name" {}
+variable "app_name" {
+  type = map(string)
+}
 
 // Domain
-variable "domain" {}
-
-// VPC
-variable "vpc_cidr_block" {}
-
-// Internet Gateway
-variable "igw_cidr_block" {}
-
-// Network ACL
-variable "nacl" {}
-
-// Subnet
-variable "subnet" {}
-
-// Security Group
-variable "security_group" {}
-
-// ECS
-variable "ecs" {}
-
-// Port
-variable "port" {}
+variable "domain" {
+  type = map(string)
+}
 
 // Key Pair
-variable "public_key" {}
+variable "public_key" {
+  type = map(string)
+}
+
+// VPC
+variable "vpc_cidr_block" {
+}
+
+// Internet Gateway
+variable "igw_cidr_block" {
+}
+
+// Network ACL
+variable "nacl" {
+  type = map(string)
+}
+
+// Subnet
+variable "subnet" {
+  type = map(string)
+}
+
+// Security Group
+variable "sg" {
+  type = map(string)
+}
+
+// Port
+variable "port" {
+  type = map(number)
+}
 
 // certificate
-variable "ssl_policy" {}
+variable "ssl_policy" {
+}
