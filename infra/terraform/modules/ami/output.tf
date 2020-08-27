0a9125f6f812f299d23cff791253bd3732b951a3
Hiroki <hasegawafeedshop@gmail.com>
2020-08-28 03:36:06 +0900

    remove Terraformのソースコードを，個別の新リポジトリに移行するに伴って，削除した．

--- infra/terraform/modules/ami/output.tf
+++
@@ -1,6 +0,0 @@
-#====================
-# Output From Module
-#====================
-output "ami_amazon_linux_2_id" {
-  value = data.aws_ami.amazon_linux_2.id
-}
--- infra/terraform/modules/route53/output.tf
+++
@@ -1,9 +0,0 @@
-#====================
-# Output From Module
-#====================
-output "route53_record_fqdn" {
-  value = aws_route53_record.route53_record.fqdn
-}
-output "route53_zone_id" {
-  value = data.aws_route53_zone.route53_zone.id
-}
